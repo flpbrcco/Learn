62c22d9f232cd1b22628cbddf16ae750e28b86ae
Filipe Barros <filipe.bc@live.com>
2022-07-02 20:25:07 -0300

    commit 20220702

---
+++ program.py
@@ -0,0 +1,41 @@
+#program.py
+sum = 1 + 2
+print(sum)
+
+print( )
+
+#função print()
+print ("show me this in the console") # "" : forma literal
+
+print( )
+
+#variáveis
+sum = 1 + 2 # 3
+product = sum * 2.5
+print(product)
+
+planets_in_solar_system = 8 #int, pluto used to be the 9th planet, but is too small
+distance_to_alpha_centauri = 4.367 #float, lightyears
+can_liftoff = True #boolean
+shuttle_landed_on_the_moon = "Apollo 11" #string
+
+print( )
+
+#operadores
+#<left side> <operator> <right side>
+left_side = 10
+right_side = 5
+left_side / right_side # 2
+
+#aritméticos ou de atribuição
+
+#datas
+from audioop import reverse
+from datetime import date
+date.today()
+print(date.today())
+
+print ( )
+
+# print("Today's date is: " + date.today()) -> erro.
+print("Today's date is: " + str(date.today()))
