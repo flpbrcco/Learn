647cd83741ecda7b07f64a78bd3a94b4e789bf02
Filipe Barros <filipe.bc@live.com>
2022-07-24 20:54:59 -0300

    09_functions

---
+++ 09_functions/exemplos.py
@@ -0,0 +1,8 @@
+from datetime import timedelta, datetime
+
+def arrival_time(destination, hours = 51):
+    now = datetime.now()
+    arrival = now + timedelta(hours = hours)
+    return arrival.strftime(f"{destination} Arrival: %A %H:%M")
+
+print(arrival_time("Orbit", hours = 0.13))
